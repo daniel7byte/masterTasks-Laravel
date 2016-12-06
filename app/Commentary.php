b23f2a731afe0b0ecc292ae36329a3e68eb9689b
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2016-12-06 15:21:09 -0500

    Migrations

---
+++ app/Commentary.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Commentary extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description',
+    ];
+
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
+}
