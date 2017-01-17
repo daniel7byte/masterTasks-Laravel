831bf2d2f42cac28d7739441c6b109a23303c2b6
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2017-01-17 18:52:22 -0500

    table-responsive

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,47 +8,49 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Tasks</div>
                     <div class="panel-body">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Title</th>
-                                    <th>Description</th>
-                                    <th>Date</th>
-                                    <th>Image</th>
-                                    @if (Auth::user()->role === "ADMIN")
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                        <th>Date</th>
+                                        <th>Image</th>
+                                        @if (Auth::user()->role === "ADMIN")
                                         <th>User</th>
-                                    @endif
-                                    <th>Created ago</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($tasks as $task)
+                                        @endif
+                                        <th>Created ago</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($tasks as $task)
                                     @can('owner', $task)
-                                        <tr>
-                                            <td>{{ $task->title }}</td>
-                                            <td>{{ $task->description }}</td>
-                                            <td>{{ \Carbon\Carbon::parse($task->date)->toDayDateTimeString() }}</td>
-                                            <td>
-                                                @if ($task->image == null)
-                                                    <span class="label label-warning">NULL</span>
-                                                @else
-                                                    <img style="max-height: 100px" class="img img-responsive img-thumbnail" src="imagesTasks/{{ $task->image }}">
-                                                @endif
-                                            </td>
-                                            @if (Auth::user()->role === "ADMIN")
-                                                <td><span class="label label-default">{{ $task->user->first_name }}</span></td>
+                                    <tr>
+                                        <td>{{ $task->title }}</td>
+                                        <td>{{ $task->description }}</td>
+                                        <td>{{ \Carbon\Carbon::parse($task->date)->toDayDateTimeString() }}</td>
+                                        <td>
+                                            @if ($task->image == null)
+                                            <span class="label label-warning">NULL</span>
+                                            @else
+                                            <img style="max-height: 100px" class="img img-responsive img-thumbnail" src="imagesTasks/{{ $task->image }}">
                                             @endif
-                                            <td>{{ \Carbon\Carbon::parse($task->created_at)->diffForHumans() }}</td>
-                                            <td>
-                                                {{ link_to_route('tasks.edit', $title = 'Edit', $parameter = $task, $attributes = ['class' => 'btn btn-xs btn-primary']) }}
-                                                @include('tasks.delete')
-                                            </td>
-                                        </tr>
+                                        </td>
+                                        @if (Auth::user()->role === "ADMIN")
+                                        <td><span class="label label-default">{{ $task->user->first_name }}</span></td>
+                                        @endif
+                                        <td>{{ \Carbon\Carbon::parse($task->created_at)->diffForHumans() }}</td>
+                                        <td>
+                                            {{ link_to_route('tasks.edit', $title = 'Edit', $parameter = $task, $attributes = ['class' => 'btn btn-xs btn-primary']) }}
+                                            @include('tasks.delete')
+                                        </td>
+                                    </tr>
                                     @endcan
-                                @endforeach
-                            </tbody>
-                        </table>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
